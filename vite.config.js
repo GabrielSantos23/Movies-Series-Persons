e07a56b0312675ca0f4dffa9f4b2ff69ccdac7f9
Gabriel Santos <33208964+gabs111@users.noreply.github.com>
2022-09-21 19:18:56 -0300

    gh-pages add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-app-sample/',
+  base: '/-gabs111-movies-series-/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
