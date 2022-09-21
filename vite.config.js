4a60084aadfb3e14875a68be5d730c77cf8fd96b
Gabriel Santos <33208964+gabs111@users.noreply.github.com>
2022-09-21 20:50:03 -0300

    teste2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Movies-Series/',
+  base: '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
