ef78f5310ea676dac2b19dd7f24fecb85f28d285
Gabriel Santos <33208964+gabs111@users.noreply.github.com>
2022-09-21 22:16:57 -0300

    teste6

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
@@ -22,11 +22,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Router>
         <CustomSwitch>
           <Route element={<App />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/movie/:id' element={<Movie />} />
-            <Route path='/serie/:id' element={<Serie />} />
+            <Route path='/Movies-Series/' element={<Home />} />
+            <Route path='Movies-Series/movie/:id' element={<Movie />} />
             <Route path='/person/:id' element={<Person />} />
-            <Route path='/MoviePages' element={<PopularMovies />} />
+            <Route path='Movies-Series/serie/:id' element={<Serie />} />
+            <Route path='MoviePages' element={<PopularMovies />} />
             <Route path='SeriesPage' element={<PopularSeries />} />
             <Route path='MovieSkeleton' element={<MovieSkeleton />} />
           </Route>
